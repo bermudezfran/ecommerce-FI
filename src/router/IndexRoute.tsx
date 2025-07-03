a2ab627063e6ae6b127770e28e117d7b5a17490c
Francisco Bermudez <bermudezfran15@gmail.com>
2025-07-03 13:59:52 -0300

    update: structure

--- src/router/IndexRoute.tsx
+++ src/router/IndexRoute.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Carrito } from "../pages/Carrito";
+import { LoginRegistro } from "../pages/LoginRegistro";
 
 export const IndexRoute = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/carrito" element={<Carrito />} />
+      <Route path="/auth" element={<LoginRegistro />} />
     </Routes>
   );
 };
